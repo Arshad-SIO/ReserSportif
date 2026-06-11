282d47450e35ac814a423361ffaed203ecd6ed7a
Arshad-SIO <arshad.umair.sio1@gmail.com>
2026-06-11 12:46:30 +0200

    added two folders for 'frontend' and 'backend'

---
+++ backend/src/main/java/com/example/resersportif/ResersportifApplication.java
@@ -0,0 +1,13 @@
+package com.example.resersportif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResersportifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResersportifApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/resersportif/ResersportifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resersportif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResersportifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
